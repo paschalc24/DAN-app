3ae9ab1191a9390fe4a191094d69fce5f87e22da
paschalc24 <pcorrigan@umass.edu>
2023-04-16 20:50:28 -0400

    Reset changes to prevent overuse of API Key.

--- src/App.js
+++ src/App.js
@@ -4,25 +4,6 @@ import { Configuration, OpenAIApi } from "openai";
 import './App.css';
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  const [firstTime, setFirstTime] = useState(true);
-  const openai = new OpenAIApi(new Configuration({
-		apiKey: process.env.REACT_APP_API_KEY
-	}))
-  openai
-  .createChatCompletion({
-  model: "gpt-3.5-turbo",
-  messages: [{
-    role: "user", 
-    content: "Hello DAN"
-  }]
-  })
-  .then(res => {
-      setMessages(res.data.choices)
-      const response = res.data.choices[0].message.content
-      setFirstTime(false)
-      console.log(response)
-  })
   return (
     <div className="App">
       <header className="App-header">
